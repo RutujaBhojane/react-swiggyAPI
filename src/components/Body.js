b1e4d7941b9581c0b1bbed50cf1ca1631f567265
Rutuja Bhojane <rutujabhojane14@gmail.com>
2025-03-02 13:51:39 +0530

    Episode 6.3 Search Functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,18 +3,15 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  //useState() is used to create state variable and it maintains the state of the component
   const [listOfRestaurants, setlistOfRestaurants] = useState([]);
 
-  //useEffect is used after your component is rendered. 
-  // Then callback function of useEffect will be executed and react will rerender the page again after fetching data from API.
+  const [filteredRestaurantList, setfilteredRestaurantList] = useState([]);
+
+  const [searchText, setsearchText] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
-
-  //The async keyword means this function will work with Promises, allowing us to use await inside it.
-  //await pauses execution until the fetch request and JSON parsing are complete.
 
   const fetchData = async () => {
     const data = await fetch(
@@ -25,40 +22,51 @@ const Body = () => {
 
     console.log(json);
     setlistOfRestaurants(
-      //optional chaining
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
-    //console.log("data fetch from api new state variable rerender");
+    setfilteredRestaurantList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
-
-  /* conditional rendering
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
-*/
-
-  //another way of conditional rendering
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setfilteredRestaurantList(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            //filter logic here
             const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
             setlistOfRestaurants(filteredList);
-            //console.log("filter button component rerender");
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
@@ -67,12 +75,3 @@ const Body = () => {
 };
 
 export default Body;
-
-/*
-1. Body component renders
-2. Then callback function of useEffect will be executed
-3. It will fetch data from API
-4. After data is received, the state variable is updated.
-5. As soon as the state variable is changed the component will be rerendered.
-6. Uncomment console statements and understand the concept.
-*/
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,7 @@ import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 
 const Header = () => {
-  //setbtnName is a js function
   const [btnName, setbtnName] = useState("Login");
-  //console.log("Header rendered again!!!");
-
-  // normal js variable does not rerender UI in line no 27 after callback function called of onclick
-  // let btnName = "Login";
 
   return (
     <div className="header">
@@ -35,7 +30,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// first react renders Header component and btnName = Login, after clicking on button on UI the onclick callback function is triggered
-// and now setbtnName sets the value of btnName to Logout and this time this btnName is new variable (React creates new instance)than older btnName
-// and after that react rerenders the Header component again.
